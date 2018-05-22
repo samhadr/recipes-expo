629c44345155064eb0c03700da85cbaf54d18667
samhadr <samhadr@gmail.com>
2018-05-22 09:49:45 -0700

    develop: update config with identity pool id

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ export default {
   },
   apiGateway: {
     REGION: "us-west-2",
-    URL: "https://8d5woxfqk1.execute-api.us-west-2.amazonaws.com/prod"
+    URL: "https://vkzg77gi44.execute-api.us-west-2.amazonaws.com/prod"
   },
   cognito: {
     REGION: "us-west-2",
     USER_POOL_ID: "us-west-2_NmrFcr4N2",
     APP_CLIENT_ID: "1dgmhuuoc8461br7qj3hs6m4ul",
-    IDENTITY_POOL_ID: "us-west-2:d892347d-7fba-4714-9fc3-2d8ca58b383c"
+    IDENTITY_POOL_ID: "us-west-2:845a24ac-6f02-4323-ac75-2f8b60e11e86"
   }
 };
